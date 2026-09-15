467edc28827159dca257d2b7d91088a7e2516cf1
Виктория Мушникова <mushnikovavikasuper@gmail.com>
2026-09-15 11:45:43 +0300

    lan04

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите сумму покупки: ");
+double summ = double.Parse(Console.ReadLine());
+if (summ > 1000)
+{
+    summ = summ - (summ * 0.1);       // скидка 10%
+}
+else if (summ > 500)
+{
+    summ = summ - (summ * 0.05); // скидка 5%
+} Console.WriteLine($"К оплате: {summ}");
+
