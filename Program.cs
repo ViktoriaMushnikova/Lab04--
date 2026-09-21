96be48542b40645af04378009e3c5dc452fdc29e
Виктория Мушникова <mushnikovavikasuper@gmail.com>
2026-09-22 02:05:15 +0300

    Lab04: Logical Operators, Practice

--- Program.cs
+++ Program.cs
@@ -13,5 +13,25 @@ if (age1 >= 18)
     Console.WriteLine($"{18 - age1} лeт осталось ждать до совершеннолетия");
 }
 
+int age2 = 70;
+if (age2 < 13)
+{
+    Console.WriteLine("Ребёнок");
+} else if (age2 < 18) {
+    Console.WriteLine("Подросток");
+} else if (age2 >= 60) {
+    Console.WriteLine("Пенсионер");
+} else {
+    Console.WriteLine("Взрослый");
+}
 
-
+int age3 = 16;
+double height = 1.4;
+bool adult = false;
+if ((age3 >= 14 && height >= 1.5) || (adult == true && height <= 1.5))
+{
+    Console.WriteLine("Можно кататься");
+} else
+{
+    Console.WriteLine("Пока нельзя");
+}
